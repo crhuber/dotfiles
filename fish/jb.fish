d0682cc0d0e04e15822d901602077f060164960d
crhuber <crhuber@users.noreply.github.com>
2025-07-15 13:06:46 +0200

    use tokyo night custom

--- fish/jb.fish
+++ fish/jb.fish
@@ -1,6 +1,6 @@
 function jb --description 'make a new jira branch from current ticket'
-   set TASK_ID (jira issue list --plain --no-headers -q"status not in ('Done', 'Canceled', 'Won\'t Do')"  -a $(jira me) |string split0 | fzf | cut -f2)
-   read --prompt "echo 'Branch suffix: $TASK_ID/' " -l branch_suffix
-   set BRANCH_NAME "$TASK_ID/$branch_suffix"
-   git checkout -b $BRANCH_NAME
+    set TASK_ID (jira issue list --plain --no-headers -q"status not in ('Done', 'Canceled', 'Won\'t Do')"  -a $(jira me) |string split0 | fzf | awk '{print $2}')
+    read --prompt "echo 'Branch suffix: $TASK_ID/' " -l branch_suffix
+    set BRANCH_NAME "$TASK_ID/$branch_suffix"
+    git checkout -b $BRANCH_NAME
 end
