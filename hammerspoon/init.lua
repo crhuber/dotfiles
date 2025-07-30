5069d3e69133772e715be6b0d3340457b906f2d8
crhuber <crhuber@users.noreply.github.com>
2025-07-30 23:34:57 +0200

    add hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,21 @@
+-- Constants
+MODIFIERS = {"ctrl", "alt"}    -- Modifiers used for app shortcuts
+
+-- App configuration
+APPS = {
+  {shortcut = "t", name = "Ghostty"},
+  {shortcut = "c", name = "Visual Studio Code"},
+  {shortcut = "b", name = "Google Chrome"},
+  {shortcut = "s", name = "Slack"},
+  {shortcut = "1", name = "Visual Studio Code"},
+  {shortcut = "2", name = "Ghostty"},
+  {shortcut = "3", name = "Google Chrome"},
+  {shortcut = "4", name = "Slack"},
+}
+
+-- Bind application shortcuts
+for _, app in ipairs(APPS) do
+  hs.hotkey.bind(MODIFIERS, app.shortcut, function()
+    hs.application.launchOrFocus(app.name)
+  end)
+end
