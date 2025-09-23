04bf1ba71504ae9eb24b537fbbbd6f7d6c030a8d
crhuber <crhuber@users.noreply.github.com>
2025-09-23 21:55:18 +0200

    add code ui

--- fish/code-ui.fish
+++ fish/code-ui.fish
@@ -1,3 +1,3 @@
 function code-ui --description 'open directory in vscode'
-   ls ~/Documents/Development/ | choose | xargs -I {} code ~/Documents/Development/{}
+   ls -t ~/Documents/Development/ | choose | xargs -I {} code ~/Documents/Development/{}
 end
