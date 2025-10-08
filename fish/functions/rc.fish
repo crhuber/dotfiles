fc77758a925a86fa39a89b5a965e2ef4f8ff3672
crhuber <crhuber@users.noreply.github.com>
2025-10-08 20:13:13 +0200

    fix rc

--- fish/functions/rc.fish
+++ fish/functions/rc.fish
@@ -1,1 +1,10 @@
-/Users/craig/.dotfiles/fish/ro-ui.fish
+function rc --description 'repo open in vscode'
+   argparse 'ui' -- $argv
+   or return
+
+   if set -q _flag_ui
+      ls -t ~/Documents/Development/ | choose | xargs -I {} code ~/Documents/Development/{}
+   else
+      ls -t ~/Documents/Development/ | fzf | xargs -I {} code ~/Documents/Development/{}
+   end
+end
