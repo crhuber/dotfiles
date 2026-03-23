c39a3cdf8ca06e1838cb7f3647c03a93d8304d45
crhuber <crhuber@users.noreply.github.com>
2026-03-23 21:04:08 +0100

    hide menu icons

--- osx.sh
+++ osx.sh
@@ -2,9 +2,9 @@
 sudo -v
 
 # Save screenshots into ~/Screenshots/ instead of desktop
-mkdir -p ~/Screenshots/
-sudo defaults write com.apple.screencapture location ~/Screenshots/
-defaults write com.apple.screencapture location ~/Screenshots/
+mkdir -p ~/Pictures/Screenshots/
+sudo defaults write com.apple.screencapture location ~/Pictures/Screenshots/
+defaults write com.apple.screencapture location ~/Pictures/Screenshots/
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
@@ -15,3 +15,6 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 
 # Finder: show all filename extensions
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Menu bar: hide icons in menus
+defaults write -g NSMenuEnableActionImages -bool NO
